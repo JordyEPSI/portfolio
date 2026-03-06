fd42d51b5ff901e7ed89a9e8a77fe76a9ea381e2
JordyEPSI <a.rahantanirina@ecoles-epsi.net>
2026-03-06 21:01:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Gère l'animation au défilement (reveal)
 window.addEventListener('scroll', reveal);
 
 function reveal() {
@@ -5,7 +6,7 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var revealTop = reveals[i].getBoundingClientRect().top;
-        var revealPoint = 150;
+        var revealPoint = 150; // Nombre de pixels avant l'apparition
 
         if (revealTop < windowHeight - revealPoint) {
             reveals[i].classList.add('active');
@@ -13,5 +14,5 @@ function reveal() {
     }
 }
 
-// Lancer une fois au chargement
-reveal();
+// Lancer une fois au chargement pour afficher la section hero
+window.addEventListener('load', reveal);
